215559898590eba1d8a48a5eb18db87acdb2e0f1
Erik May <may.erik@gmail.com>
2016-01-02 17:31:31 -0800

    removing compiled javascript

--- app/backend/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express_1 = require('express');
-var index = express_1.Router();
-/* GET home page. */
-index.get('/', function (req, res, next) {
-    res.render('index');
-});
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = index;
