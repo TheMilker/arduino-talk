2d06375e07116a77ba594c9b8ed695ab0af3f414
Erik May <may.erik@gmail.com>
2016-01-17 19:09:40 -0800

    removing socket.io until i can find a better way to do video streaming. attempting to support multiple server platforms for streaming. code cleanup

--- app/backend/src/bin/www.ts
+++ app/backend/src/bin/www.ts
@@ -1,8 +1,8 @@
 import app from '../app';
 import * as debugModule from 'debug';
 import * as http from 'http';
-import * as sio from 'socket.io';
 import * as childProcess from 'child_process';
+import * as os from 'os';
 let ws = require('ws');
 
 const debug = debugModule('node-express-typescript:server');
@@ -21,54 +21,36 @@ var STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
 var width = 320;
 var height = 240;
 
-// const io = sio(server);
-// io.on('connection', (socket) =>{
-//     // Send magic bytes and video size to the newly connected socket
-//     // struct { char magic[4]; unsigned short width, height;}
-//     var streamHeader = new Buffer(8);
-
-//     streamHeader.write(STREAM_MAGIC_BYTES);
-//     streamHeader.writeUInt16BE(width, 4);
-//     streamHeader.writeUInt16BE(height, 6);
-//     socket.send(streamHeader, { binary: true });
-
-//     console.log('New WebSocket Connection (' + io.sockets.sockets.length + ' total)');
-
-//     socket.on('disconnect', (s) => {
-//         console.log('Disconnected WebSocket (' + io.sockets.sockets.length + ' total)');
-//     });
-// });
-
 // WebSocket server
 var wsServer = new (ws.Server)({ port: 8084 });
 console.log('WebSocket server listening on port ' + 8084);
 
 wsServer.on('connection', function(socket) {
-  // Send magic bytes and video size to the newly connected socket
-  // struct { char magic[4]; unsigned short width, height;}
-  var streamHeader = new Buffer(8);
+    // Send magic bytes and video size to the newly connected socket
+    // struct { char magic[4]; unsigned short width, height;}
+    var streamHeader = new Buffer(8);
 
-  streamHeader.write(STREAM_MAGIC_BYTES);
-  streamHeader.writeUInt16BE(width, 4);
-  streamHeader.writeUInt16BE(height, 6);
-  socket.send(streamHeader, { binary: true });
+    streamHeader.write(STREAM_MAGIC_BYTES);
+    streamHeader.writeUInt16BE(width, 4);
+    streamHeader.writeUInt16BE(height, 6);
+    socket.send(streamHeader, { binary: true });
 
-  console.log('New WebSocket Connection (' + wsServer.clients.length + ' total)');
+    console.log('New WebSocket Connection (' + wsServer.clients.length + ' total)');
 
-  socket.on('close', function(code, message){
-    console.log('Disconnected WebSocket (' + wsServer.clients.length + ' total)');
-  });
+    socket.on('close', function(code, message) {
+        console.log('Disconnected WebSocket (' + wsServer.clients.length + ' total)');
+    });
 });
 
 wsServer.broadcast = function(data, opts) {
-  for(var i in this.clients) {
-    if(this.clients[i].readyState == 1) {
-      this.clients[i].send(data, opts);
+    for (var i in this.clients) {
+        if (this.clients[i].readyState == 1) {
+            this.clients[i].send(data, opts);
+        }
+        else {
+            console.log('Error: Client (' + i + ') not connected.');
+        }
     }
-    else {
-      console.log('Error: Client (' + i + ') not connected.');
-    }
-  }
 };
 
 server.listen(port);
@@ -78,100 +60,95 @@ server.on('listening', onListening);
 /**
  * Normalize a port into a number, string, or false.
  */
-function normalizePort(val: any): number|string|boolean {
-  let port = parseInt(val, 10);
+function normalizePort(val: any): number | string | boolean {
+    let port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
 
-  return false;
+    return false;
 }
 
 /**
  * Event listener for HTTP server "error" event.
  */
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
-  let bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 function onListening() {
-  let addr = server.address();
-  let bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
 
-  debug('Listening on ' + bind);
+    debug('Listening on ' + bind);
 }
 
-// HTTP server to accept incoming MPEG1 stream
-// http.createServer((req, res) => {
-//     console.log(
-//         'Stream Connected: ' + req.socket.remoteAddress +
-//         ':' + req.socket.remotePort + ' size: ' + width + 'x' + height
-//     );
-
-//     req.on('data', function (data) {
-//         io.emit('camera data', data);
-//     });
-// }).listen(8082, () => {
-//     console.log('Listening for video stream on port ' + 8082);
-                        
-//     childProcess.exec('ffmpeg -s 320x240 -f vfwcap -i /dev/video0 -f mpeg1video -b 800k -r 30 http://127.0.0.1:8082', execCallback);
-// });
-
 function execCallback(error, stdout, stderr) {
-   if (error) {
-     console.log(error.stack);
-     console.log('Error code: '+error.code);
-     console.log('Signal received: '+error.signal);
-   }
-   console.log('Child Process STDOUT: '+stdout);
-   console.log('Child Process STDERR: '+stderr);
- };
+    if (error) {
+        console.log(error.stack);
+        console.log('Error code: ' + error.code);
+        console.log('Signal received: ' + error.signal);
+    }
+    console.log('Child Process STDOUT: ' + stdout);
+    console.log('Child Process STDERR: ' + stderr);
+};
 
 // HTTP server to accept incoming MPEG1 stream
-http.createServer(function (req, res) {
-  console.log(
-    'Stream Connected: ' + req.socket.remoteAddress +
-    ':' + req.socket.remotePort + ' size: ' + width + 'x' + height
-  );
+http.createServer(function(req, res) {
+    console.log(
+        'Stream Connected: ' + req.socket.remoteAddress +
+        ':' + req.socket.remotePort + ' size: ' + width + 'x' + height
+    );
 
-  req.on('data', function (data) {
-    wsServer.broadcast(data, { binary: true });
-  });
-}).listen(8082, function () {
-  console.log('Listening for video stream on port ' + 8082);
+    req.on('data', function(data) {
+        wsServer.broadcast(data, { binary: true });
+    });
+}).listen(8082, function() {
+    console.log('Listening for video stream on port ' + 8082);
 
-  // Run do_ffmpeg.sh from node                                                   
-  childProcess.exec('ffmpeg -s 320x240 -f vfwcap -i /dev/video0 -f mpeg1video -b 800k -r 30 http://127.0.0.1:8082', execCallback);
+    let codec;        
+    switch (os.platform()) {
+    case 'win32':
+        codec = 'vfwcap';
+        break;
+    case 'linux':
+        codec = 'video4linux2';
+        break;
+    case 'darwin':
+        codec = 'qtkit';
+        break;
+}
+    childProcess.exec('ffmpeg -s 320x240 -f '+ codec +' -i /dev/video0 -f mpeg1video -b 800k -r 30 http://127.0.0.1:8082', execCallback);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,6 @@ gulp.task('deploy:frontendLibraries', () => {
         rootPaths.node + 'angular2/bundles/angular2.dev.js',
         rootPaths.node + 'systemjs/dist/system.js',
         rootPaths.node + 'systemjs/dist/system.js.map',
-        rootPaths.node + 'socket.io-client/socket.io.js',
         rootPaths.node + 'jsmpeg/jsmpg.js',
     ])
     .pipe(gulp.dest(paths.lib.dest));
